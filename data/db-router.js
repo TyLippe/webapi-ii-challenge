7e16eccb54baa2c9de2c887cfa1dd50efc909ebf
Ty Lippe <tylippe94@gmail.com>
2019-07-09 18:03:50 -0400

    added all endpoints

--- data/db-router.js
+++ data/db-router.js
@@ -6,13 +6,102 @@ const router = express.Router();
 
 router.use(express.json());
 
-//Post blog
+//Post
 router.post('/', (req, res) => {
-    const newInfo = req.body;
+    const newPost = req.body;
 
-    Data.insert(newInfo)
-        .then(info => {
-            res.status(201).json(info)
+    Data.insert(newPost)
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+//Post comment 
+router.post('/:id/comments', (req, res) => {
+    const newComment = req.body
+
+    Data.insertComment(newComment)
+        .then(comment => {
+            res.status(201).json(comment)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+//Get posts
+router.get('/', (req, res) => {
+    Data.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+//Get by id
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Data.findById(id)
+        .then(found => {
+            if (found) {
+                res.status(200).json(found)
+            } else {
+                res.status(404).json({ message: 'ID not found' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+//Get comments
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+
+    Data.findPostComments(id)
+        .then(found => {
+            if (found) {
+                res.status(200).json(found)
+            } else {
+                res.status(404).json({ message: 'ID not found' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+//Delete post
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Data.remove(id)
+        .then(deleted => {
+            if (deleted) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ message: 'ID not found.' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+//Put update
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updates = req.body;
+
+    Data.update(id, updates)
+        .then(update =>  {
+            res.status(201).json(update)
         })
         .catch(err => {
             res.status(500).json(err)
